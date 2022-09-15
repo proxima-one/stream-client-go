647971392b3ade53001296f7ec75b87cf2bda22f
OS-M <manalog44@gmail.com>
2022-09-15 15:07:32 +0200

    added more getters to ProximaStreamObject

--- model/stream_object.go
+++ model/stream_object.go
@@ -1,5 +1,7 @@
 package model
 
+import "time"
+
 type ProximaStreamObject struct {
 	Transition *Transition `json:"transition"`
 	Preprocess *TransitionPreprocessingResult
@@ -11,4 +13,12 @@ func (o *ProximaStreamObject) GetTransition() *Transition {
 
 func (o *ProximaStreamObject) GetPreprocess() *TransitionPreprocessingResult {
 	return o.Preprocess
+}
+
+func (o *ProximaStreamObject) GetTimestamp() time.Time {
+	return o.Transition.Event.Timestamp
+}
+
+func (o *ProximaStreamObject) GetUndo() bool {
+	return o.Transition.Event.Undo
 }
