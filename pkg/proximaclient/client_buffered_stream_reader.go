09807dd35a41aa065c225832a7aa05d617ed3c9d
OS-M <manalog44@gmail.com>
2022-12-15 14:01:07 +0100

    BufferedStreamReader eof logic

--- pkg/proximaclient/client_buffered_stream_reader.go
+++ pkg/proximaclient/client_buffered_stream_reader.go
@@ -19,7 +19,10 @@ func (b *BufferedStreamReader) TryRead(maxBatchSize int) []StreamEvent {
 	res = append(res, event)
 	for i := 1; i < maxBatchSize; i++ {
 		select {
-		case event := <-b.stream:
+		case event, ok := <-b.stream:
+			if !ok {
+				return res
+			}
 			res = append(res, event)
 		default:
 			return res
